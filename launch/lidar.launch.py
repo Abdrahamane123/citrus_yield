4693c14d649f90dfc4ffcfde68be166634169d6a
Abdrahamane123 <a.koitekoite@gmail.com>
2024-10-05 21:17:42 +0100

    v1

---
+++ launch/lidar.launch.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import LogInfo
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    channel_type =  LaunchConfiguration('channel_type', default='serial')
+    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
+    serial_baudrate = LaunchConfiguration('serial_baudrate', default='460800')
+    frame_id = LaunchConfiguration('frame_id', default='laser_frame')
+    inverted = LaunchConfiguration('inverted', default='false')
+    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+    scan_mode = LaunchConfiguration('scan_mode', default='Standard')
+
+    return LaunchDescription([
+
+        Node(
+            package='sllidar_ros2',
+            executable='sllidar_node',
+            output='screen',
+            name='sllidar_node',
+            parameters=[{'channel_type':channel_type,
+                         'serial_port': serial_port, 
+                         'serial_baudrate': serial_baudrate, 
+                         'frame_id': frame_id,
+                         'inverted': inverted, 
+                         'angle_compensate': angle_compensate,
+                           'scan_mode': scan_mode
+                         }],
+        )
+    ])
