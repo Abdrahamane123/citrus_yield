6471a0882733bc821635af5a54f4ebee070b3ef6
Abdrahamane123 <a.koitekoite@gmail.com>
2024-10-07 03:48:57 +0100

    v0

--- launch/lidar.launch.py
+++ launch/lidar.launch.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -11,7 +9,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     channel_type =  LaunchConfiguration('channel_type', default='serial')
-    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
     serial_baudrate = LaunchConfiguration('serial_baudrate', default='460800')
     frame_id = LaunchConfiguration('frame_id', default='laser_frame')
     inverted = LaunchConfiguration('inverted', default='false')
@@ -26,7 +23,7 @@ def generate_launch_description():
             output='screen',
             name='sllidar_node',
             parameters=[{'channel_type':channel_type,
-                         'serial_port': serial_port, 
+                         'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0', 
                          'serial_baudrate': serial_baudrate, 
                          'frame_id': frame_id,
                          'inverted': inverted, 
